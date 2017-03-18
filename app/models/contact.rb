31503bedccfaf530466c2c7a010d20ed4d69fa6e
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-03-17 21:24:29 -0300

    add dependent delete to models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ActiveRecord::Base
   belongs_to :user
-  has_many :text_field_values
+  has_many :text_field_values, dependent: :delete_all
   accepts_nested_attributes_for :text_field_values
-  has_many :text_area_field_values
+  has_many :text_area_field_values, dependent: :delete_all
   accepts_nested_attributes_for :text_area_field_values
 end
--- app/models/field.rb
+++ app/models/field.rb
@@ -1,5 +1,7 @@
 class Field < ActiveRecord::Base
   belongs_to :user
+  has_many :text_field_values, dependent: :delete_all
+  has_many :text_area_field_values, dependent: :delete_all
 
   enum field_type: [:text, :text_area]
 
